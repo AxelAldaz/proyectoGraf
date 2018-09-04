5d3c658c28f027b4c488960262707006f9fff72d
Axel Castillo Aldaz <axl_castillo@hotmail.com>
2018-09-04 18:11:56 -0500

    Hola Mundo

--- proyecto2/src/proyecto2/Proyecto2.java
+++ proyecto2/src/proyecto2/Proyecto2.java
@@ -16,7 +16,8 @@ public class Proyecto2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Cambios");
+        System.out.println("Hola mundo");
+    
     }
     
 }
